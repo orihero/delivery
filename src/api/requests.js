57f5891b4b6c0f24fa0f2597c1d1f77b98babd6a
Abdumutal Abdusamatov <mebungler@gmail.com>
2021-10-13 18:56:23 +0500

    Save Complete

--- src/api/requests.js
+++ src/api/requests.js
@@ -1,45 +1,42 @@
-import Axios from "axios";
+import Axios from 'axios';
+import { store } from '../store/configureStore';
 
-export let url = "http://192.168.101.209:8080";
-
-let user = localStorage.getItem("user") || "";
-let parsedUser = {};
-try {
-  parsedUser = JSON.parse(user);
-} catch (error) {}
+export let url = 'http://192.168.101.209:8080';
 
 let axios = Axios.create({
-  baseURL: url,
+	baseURL: url,
 });
 
 axios.interceptors.request.use((e) => {
-  if (!!parsedUser.token) {
-    e.headers = { ...e.headers, ["Token"]: parsedUser.token };
-  }
-  return e;
+	let user = store.getState().user;
+	if (!!user.token) {
+		e.headers = { ...e.headers, ['Token']: user.token };
+	}
+	return e;
 });
 
 axios.interceptors.response.use(
-  (e) => e,
-  (e) => {
-    if (e.response && e.response.status === 401) {
-      localStorage.clear();
-      window.location = "/login";
-    }
-    return e;
-  }
+	(e) => e,
+	(e) => {
+		if (e.response && e.response.status === 401) {
+			localStorage.clear();
+			window.location = '/login';
+		}
+		return e;
+	}
 );
 
 export let requests = {
-  auth: {
-    login: (credentials) => axios.post("/signin", credentials),
-    register: (credentials) => axios.post("/signup", credentials),
-  },
-  restaurants: {
-    getRestaurants: () => axios.get("/restaurant"),
-    createRestaurant: (credentials) => axios.post("/restaurant", credentials),
-  },
-  file: {
-    upload: (data) => axios.post("/upload", data),
-  },
+	auth: {
+		login: (credentials) => axios.post('/signin', credentials),
+		register: (credentials) => axios.post('/signup', credentials),
+	},
+	restaurants: {
+		getRestaurants: () => axios.get('/restaurant'),
+		createRestaurant: (credentials) =>
+			axios.post('/restaurant', credentials),
+	},
+	file: {
+		upload: (data) => axios.post('/upload', data),
+	},
 };
--- src/screens/shop/shop.jsx
+++ src/screens/shop/shop.jsx
@@ -1,56 +1,67 @@
-import React, { useEffect, useState } from "react";
-import { requests } from "../../api/requests";
-import FeatherIcon from "feather-icons-react";
-import "./shop.css";
+import React, { useEffect, useState } from 'react';
+import { requests } from '../../api/requests';
+import FeatherIcon from 'feather-icons-react';
+import './shop.css';
+import { url as remoteIP } from '../../api/requests';
 
 export default function Shop({ history }) {
-  const [items, setItems] = useState([]);
-  let effect = async () => {
-    try {
-      let res = await requests.restaurants.getRestaurants();
-      setItems(res.data);
-    } catch (error) {}
-  };
-  useEffect(() => {
-    effect();
-  }, []);
-  function onEditClick(item) {
-    history.push({pathname:"/restaurant-edit", state:{item}});
-  }
-  function onadd(){
-    history.push("/add-restaurant")
-  }
-  return (
-    <div className="item-container">
-      <div className="add-container">
-      <button className="add" onClick={onadd}>Add Restaurant</button>
-      </div>
-      {items?.map((e, i) => {
-        return (
-          
-          <div className="item" key={i}>
-            <img src={e.photo_url} />
-            <div className="foodsname">
-              <div className="names">
-                <h1>{e.name}</h1>
-                {e.description}
-              </div>
-              <div className="button">
-                <button>
-                  {" "}
-                  <FeatherIcon icon={"trash-2"} />
-                  <p>Dalete</p>
-                </button>
-                <button>
-                  {" "}
-                  <FeatherIcon icon={"edit"} />
-                  <p onClick={()=>onEditClick(e)}>Edit</p>{" "}
-                </button>
-              </div>
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
+	const [items, setItems] = useState([]);
+	let effect = async () => {
+		try {
+			let res = await requests.restaurants.getRestaurants();
+			setItems(res.data);
+		} catch (error) {}
+	};
+	useEffect(() => {
+		effect();
+	}, []);
+	function onEditClick(item) {
+		history.push({ pathname: '/restaurant-edit', state: { item } });
+	}
+	function onadd() {
+		history.push('/add-restaurant');
+	}
+
+	return (
+		<div className='item-container'>
+			<div className='add-container'>
+				<button className='add' onClick={onadd}>
+					Add Restaurant
+				</button>
+			</div>
+			{items?.map((e, i) => {
+				let url = e.photo_url;
+				console.log({ url });
+				if (!!url && url.indexOf('http') === -1) {
+					url = `${remoteIP}/download/${url}`;
+				}
+				console.log({ newUrl: url });
+				return (
+					<div className='item' key={i}>
+						<img src={url} alt={e.name} />
+						<div className='foodsname'>
+							<div className='names'>
+								<h1>{e.name}</h1>
+								{e.description}
+							</div>
+							<div className='button'>
+								<button>
+									{' '}
+									<FeatherIcon icon={'trash-2'} />
+									<p>Dalete</p>
+								</button>
+								<button>
+									{' '}
+									<FeatherIcon icon={'edit'} />
+									<p onClick={() => onEditClick(e)}>
+										Edit
+									</p>{' '}
+								</button>
+							</div>
+						</div>
+					</div>
+				);
+			})}
+		</div>
+	);
 }
